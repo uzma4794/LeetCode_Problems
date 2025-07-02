4a35fc79ade003dde6adbe423bc9327b7b4ade86
Uzma Waheed <136222729+uzma4794@users.noreply.github.com>
2025-07-02 22:09:40 +0500

    Add files via upload

---
+++ 283 - MoveZeros.py
@@ -0,0 +1,9 @@
+        l=0
+        for r in range(len(nums)):   
+            if nums[r]: 
+                
+                nums[r] , nums[l ] = nums[l] ,  nums[r]
+                l+=1    
+        return nums
+        
+        
