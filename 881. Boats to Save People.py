c43faea74d6db9d636819721af149c611b754a4f
Uzma Waheed <136222729+uzma4794@users.noreply.github.com>
2025-07-10 00:05:45 +0500

    Update 881. Boats to Save People.py

--- 881. Boats to Save People.py
+++ 881. Boats to Save People.py
@@ -1,16 +1,16 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
        
-        people.sort()
+        people.sort()              # this part sorts the array by 0(nlog n ) time complexity 
 
         boats = 0
-        
+                           
         left, right = 0, len(people)-1
 
-        while left <= right:
-            remaining = limit - people[right]
+        while left <= right:             
+            remaining = limit - people[right]                #adjust heavy weight poeple first 
             right -= 1
             boats += 1
-            if left<=right and remaining >= people[left]:
+            if left<=right and remaining >= people[left]:       # if there is capacity to enter people add them 
                 left +=1
         return boats
