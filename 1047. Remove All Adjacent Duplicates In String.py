33f21906381e0f7123e38cbf59468aae5a2407d1
Uzma Waheed <136222729+uzma4794@users.noreply.github.com>
2025-07-22 13:15:10 +0500

    Add files via upload

---
+++ 1047. Remove All Adjacent Duplicates In String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack =[]
+        for char in s:
+            if not stack: 
+                stack.append(char)
+            else:
+                if char == stack[-1]:
+                    stack.pop()
+                else:
+                    stack.append(char)
+        return "".join(stack)
